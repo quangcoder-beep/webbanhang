2d7e69365f6cbc7324f25b96bc9dda4ee6fd9d57
Lai Vinh Quang <laiquangteo123@gmail.com>
2025-09-25 22:57:06 +0700

    pecfect

--- shop/views.py
+++ shop/views.py
@@ -2,4 +2,4 @@ from django.shortcuts import render
 
 # Create your views here.
 def bodyhome(request):
-    return render(request, 'shop/headerhome.html')
+    return render(request, 'shop/bodyhome.html')
