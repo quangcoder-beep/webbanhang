74ab0003a27be9a33e36a676b95bb9775ebbea34
Lai Vinh Quang <laiquangteo123@gmail.com>
2025-10-23 02:43:49 +0700

    update xong 2

--- shop/views.py
+++ shop/views.py
@@ -1,16 +1,20 @@
 from django.shortcuts import render
 
 # Create your views here.
-def bodyhome(request, page = 1):
-    return render(request, 'shop/bodyhome.html', {"page": page})
+def bodyhome(request, page=1):
+    return render(request, 'shop/contenbody/home/bodyhome.html', {"page": page})
+
 def login(request):
     return render(request, 'account/login.html')
+
 def logup(request):
     return render(request, 'account/lopup.html')
 
 def phimdangchieu(request):
-    return render(request, 'shop/bodyhome_phimdangchieu.html')
+    return render(request, 'shop/contenbody/phimdangchieu/bodyhome_phimdangchieu.html')
+
 def phimle(request):
-    return render(request, 'shop/bodyhome_phimle.html')
+    return render(request, 'shop/contenbody/phimle/bodyhome_phimle.html')
+
 def gioithieu(request):
-    return render(request, 'shop/gioithieu.html')
+    return render(request, 'shop/contenbody/gioithieu/gioithieu.html')
