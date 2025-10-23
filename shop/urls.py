4e74e3769b044ead53e3460b6364d5a00339feaa
Lai Vinh Quang <laiquangteo123@gmail.com>
2025-10-24 03:44:18 +0700

    update responsive moi nhat

--- shop/urls.py
+++ shop/urls.py
@@ -3,16 +3,23 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
+    # Admin
     path('admin/', admin.site.urls),
-
-    path('', views.bodyhome, name='home'),
-    path('<int:page>/', views.bodyhome, name='home'),
-    path('<str:s>/', views.bodyhome, name='home'),
     
+    # Trang chủ
+    path('', views.bodyhome, name='home'),
+    
+    # Các URL cụ thể
     path('phimdangchieu/', views.phimdangchieu, name='phimdangchieu'),
     path('phimdangchieu/<str:s>/', views.phimdangchieu, name='phimdangchieu'),
+
     path('phimle/', views.phimle, name='phimle'),
     path('phimle/<str:s>/', views.phimle, name='phimle'),
+    
     path('gioithieu/', views.gioithieu, name='gioithieu'),
     path('gioithieu/<str:s>/', views.gioithieu, name='gioithieu'),
+    
+    # Các URL có tham số (để cuối cùng)
+    path('<int:page>/', views.bodyhome, name='home'),
+    path('<str:s>/', views.bodyhome, name='home'),
 ]
